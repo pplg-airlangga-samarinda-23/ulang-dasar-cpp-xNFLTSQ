eaf926471fe96088804d6cc57a1a26083cc6a7a0
muhammad naufal tsaqif <zyrexxnaufal@gmail.com>
2024-07-31 03:35:28 +0800

    tugas selesai

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 
+void cetakData(string nama, string nisn, string jurusan, int tinggiBadan)
+{
+    cout << "nama :" << nama << endl;
+    cout << "nisn :" << nisn << endl; 
+    cout << "jurusan :" << jurusan << endl;
+    cout << "tinggi badan :" << tinggiBadan << endl;
+    
+}
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama;
+    string nisn;
+    string jurusan;
+    int tinggiBadan;
+
+    cout << "Masukkan nama: ";
+    cin >> nama;
+    cout << "Masukan nisn: ";
+    cin >> nisn;
+    cout << "Masukan jurusan: ";
+    cin >> jurusan;
+    cout << "Masukan tinggiBadan : ";
+    cin >> tinggiBadan;
+
+    cetakData(nama, nisn, jurusan, tinggiBadan);
+
     return 0;
 }
